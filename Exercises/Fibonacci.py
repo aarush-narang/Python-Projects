4c83069cd36fef40e3943ffdacc2f9c9f7383689
aarush-narang <aarushnarang@gmail.com>
2022-01-10 16:01:07 -0800

    cache decorator is amazing

--- Exercises/Fibonacci.py
+++ Exercises/Fibonacci.py
@@ -1,4 +1,11 @@
-import math
+from functools import cache
+import time
+
+@cache # holy shit this thing makes it fast
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n - 1) + fib(n - 2)
 
 numbers = []
 previousnumber = 0
@@ -9,20 +16,15 @@ except ValueError:
     print('Please enter an integer')
     exit(0)
 
+start = time.time()
 
 if numberofnumbers < 0:
     print('Please enter an integer greater than or equal to 0')
-elif numberofnumbers == 0:
+else:
     numbers = []
-elif numberofnumbers == 1:
-    numbers = [1]
-elif numberofnumbers == 2:
-    numbers = [1, 1]
-elif numberofnumbers > 2:
-    numbers = [1, 1]
-    for x in range(numberofnumbers-2):
-        numbers.append(numbers[x]+numbers[x+1])
+    for x in range(numberofnumbers):
+        numbers.append(fib(x))
 
-str_numbers = [str(int) for int in numbers] # converts ints to strings so it can be joined
-
-print(', '.join(str_numbers))
+print(numbers)
+end = time.time()
+print(f'\n{end-start} sec')
